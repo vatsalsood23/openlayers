19eda273026eaeb026f2b05e6370dba621eef61f
vatsalsood23 <vsood@metocean.com>
2020-11-09 08:59:18 -0400

    render map

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 const init = () => {
-  const map = new ol.Map({});
+  const map = new ol.Map({
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    target: "js-map",
+  });
 };
+
+window.onload = init;
