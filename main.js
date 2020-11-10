e761ad4c4a4e8427aa3bcb2ef024561b8b7614f0
vatsalsood23 <vsood@metocean.com>
2020-11-10 11:39:35 -0400

    add layers and attributions

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const init = () => {
+  const attributionControl = new ol.control.Attribution({
+    collapsible: true,
+  });
+
   const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
@@ -26,6 +30,9 @@ const init = () => {
     ],
     target: "js-map",
     keyboardEventTarget: document,
+    controls: ol.control
+      .defaults({ atrribution: false })
+      .extend([attributionControl]),
   });
 
   // Layer Group
@@ -48,19 +55,70 @@ const init = () => {
   });
   map.addLayer(layerGroup);
 
+  // Carto DB layer
   const cartoDBBaseLayer = new ol.layer.Tile({
     source: new ol.source.XYZ({
       url:
         "https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{scale}.png",
+      attributions: "Cato",
     }),
     visible: true,
   });
 
   map.addLayer(cartoDBBaseLayer);
 
-  map.on("click", (e) => {
-    console.log(e.coordinate);
+  // Stamen basemap layer
+  const stamenBaseLayer = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+      layer: "terrain-labels",
+    }),
+    visible: false,
   });
+
+  map.addLayer(stamenBaseLayer);
+
+  // Stamen basemap layer
+  const stamenBaseMapLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url: "http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg",
+    }),
+    visible: false,
+  });
+
+  map.addLayer(stamenBaseMapLayer);
+
+  //   tile ArcGIS REST API Layer
+  const tileArcGISLayer = new ol.layer.Tile({
+    source: new ol.source.TileArcGISRest({
+      url:
+        "https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer",
+    }),
+    visible: false,
+  });
+  map.addLayer(tileArcGISLayer);
+
+  //   NOAA WMS Layer
+  const NOAAWMSLayer = new ol.layer.Tile({
+    source: new ol.source.TileWMS({
+      url:
+        "https://nowcoast.noaa.gov/arcgis/services/nowcoast/analysis_meteohydro_sfc_rtma_time/MapServer/WMSServer",
+      params: {
+        LAYERS: 2,
+        FORMAT: "image/png",
+        TRANSPARENT: true,
+      },
+      attributions: '<a href="https://nowcoast.noaa.gov/">copyright noa</a>',
+    }),
+    visible: false,
+  });
+
+  map.addLayer(NOAAWMSLayer);
+
+  console.log(
+    NOAAWMSLayer.getSource().setAttributions(
+      '<a href="https://nowcoast.noaa.gov/">copyright noa</a>'
+    )
+  );
 };
 
 window.onload = init;
