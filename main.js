f8d3ed97d9770e486aff72b4335bd9d0145f8283
vatsalsood23 <vsood@metocean.com>
2020-11-10 08:43:04 -0400

    add extent and tiles

--- main.js
+++ main.js
@@ -1,81 +1,65 @@
 const init = () => {
-  // Import Controls
-  const fullScreenControl = new ol.control.FullScreen();
-  const mousePositionControl = new ol.control.MousePosition();
-  const overviewMapControl = new ol.control.OverviewMap({
-    collapsed: false,
-    layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
-  });
-  const scaleLineControl = new ol.control.ScaleLine();
-  const zoomSliderControl = new ol.control.ZoomSlider();
-  const zoomToExtentControl = new ol.control.ZoomToExtent();
-
   const map = new ol.Map({
     view: new ol.View({
       center: [0, 0],
       zoom: 3,
-      maxZoom: 6,
-      minZoom: 2,
+      //   extent: [
+      //     7200979.560689885,
+      //     675091.8338146787,
+      //     10860172.978757843,
+      //     4412556.768846657,
+      //   ],
     }),
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM(),
+        zIndex: 1,
+        visible: false,
+        // Only this area of this layer will be visible
+        // extent: [
+        //   7200979.560689885,
+        //   675091.8338146787,
+        //   10860172.978757843,
+        //   4412556.768846657,
+        // ],
       }),
     ],
     target: "js-map",
     keyboardEventTarget: document,
-    controls: ol.control
-      .defaults()
-      .extend([
-        fullScreenControl,
-        mousePositionControl,
-        overviewMapControl,
-        scaleLineControl,
-        zoomSliderControl,
-        zoomToExtentControl,
-      ]),
   });
 
-  // Coordinates overlay
-  const popupContainerElement = document.getElementById("popup-coordinates");
-  const popup = new ol.Overlay({
-    element: popupContainerElement,
-    positioning: "center-left",
+  // Layer Group
+  const layerGroup = new ol.layer.Group({
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM({
+          url: "https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        }),
+        zIndex: 0,
+        visible: false,
+        // extent: [
+        //   7200979.560689885,
+        //   675091.8338146787,
+        //   10860172.978757843,
+        //   4412556.768846657,
+        // ],
+      }),
+    ],
+  });
+  map.addLayer(layerGroup);
+
+  const cartoDBBaseLayer = new ol.layer.Tile({
+    source: new ol.source.XYZ({
+      url:
+        "https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{scale}.png",
+    }),
+    visible: true,
   });
 
-  map.addOverlay(popup);
+  map.addLayer(cartoDBBaseLayer);
 
   map.on("click", (e) => {
-    const clickedCoordinate = e.coordinate;
-    popup.setPosition(undefined);
-    popup.setPosition(clickedCoordinate);
-    popupContainerElement.innerHTML = clickedCoordinate;
-  });
-
-  // DragRotate Interaction
-  const dragRotateInteraction = new ol.interaction.DragRotate({
-    condition: ol.events.condition.altKeyOnly,
-  });
-
-  map.addInteraction(dragRotateInteraction);
-
-  // Draw Interaction
-  const drawInteraction = new ol.interaction.Draw({
-    type: "Polygon",
-    freehand: true,
-  });
-
-  map.addInteraction(drawInteraction);
-
-  // Could use the following code to save the drawing to the db after we finish drawing
-  drawInteraction.on("drawend", (e) => {
-    console.log("drawend", e);
-    let parser = new ol.format.GeoJSON();
-    let drawnFeatures = parser.writeFeaturesObject([e.feature]);
-    console.log(
-      "drawnFeatures",
-      drawnFeatures.features[0].geometry.coordinates[0]
-    );
+    console.log(e.coordinate);
   });
 };
 
---
+++ main_old.js
@@ -0,0 +1,83 @@
+const init = () => {
+    // Import Controls
+    const fullScreenControl = new ol.control.FullScreen();
+    const mousePositionControl = new ol.control.MousePosition();
+    const overviewMapControl = new ol.control.OverviewMap({
+      collapsed: false,
+      layers: [new ol.layer.Tile({ source: new ol.source.OSM() })],
+    });
+    const scaleLineControl = new ol.control.ScaleLine();
+    const zoomSliderControl = new ol.control.ZoomSlider();
+    const zoomToExtentControl = new ol.control.ZoomToExtent();
+  
+    const map = new ol.Map({
+      view: new ol.View({
+        center: [0, 0],
+        zoom: 3,
+        maxZoom: 12,
+        minZoom: 2,
+      }),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM(),
+        }),
+      ],
+      target: "js-map",
+      keyboardEventTarget: document,
+      controls: ol.control
+        .defaults()
+        .extend([
+          fullScreenControl,
+          mousePositionControl,
+          overviewMapControl,
+          scaleLineControl,
+          zoomSliderControl,
+          zoomToExtentControl,
+        ]),
+    });
+  
+    // Coordinates overlay
+    const popupContainerElement = document.getElementById("popup-coordinates");
+    const popup = new ol.Overlay({
+      element: popupContainerElement,
+      positioning: "center-left",
+    });
+  
+    map.addOverlay(popup);
+  
+    map.on("click", (e) => {
+      const clickedCoordinate = e.coordinate;
+      popup.setPosition(undefined);
+      popup.setPosition(clickedCoordinate);
+      popupContainerElement.innerHTML = clickedCoordinate;
+    });
+  
+    // DragRotate Interaction
+    const dragRotateInteraction = new ol.interaction.DragRotate({
+      condition: ol.events.condition.altKeyOnly,
+    });
+  
+    map.addInteraction(dragRotateInteraction);
+  
+    // Draw Interaction
+    const drawInteraction = new ol.interaction.Draw({
+      type: "Polygon",
+      freehand: true,
+    });
+  
+    map.addInteraction(drawInteraction);
+  
+    // Could use the following code to save the drawing to the db after we finish drawing
+    drawInteraction.on("drawend", (e) => {
+      console.log("drawend", e);
+      let parser = new ol.format.GeoJSON();
+      let drawnFeatures = parser.writeFeaturesObject([e.feature]);
+      console.log(
+        "drawnFeatures",
+        drawnFeatures.features[0].geometry.coordinates[0]
+      );
+    });
+  };
+  
+  window.onload = init;
+  
