bb419a4cc4bf030c11b87e73b5b18df40065dfd2
vatsalsood23 <vsood@metocean.com>
2020-11-16 10:59:53 -0400

    WIP add rectangle to existing rectangle

--- main_image.js
+++ main_image.js
@@ -1,3 +1,8 @@
+var VectorSource = ol.source.Vector;
+var { Tile, Vector } = ol.layer; //import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+var TileLayer = Tile;
+var VectorLayer = Vector;
+
 const Feature = ol.Feature;
 const Point = ol.geom.Point;
 const init = () => {
@@ -26,7 +31,6 @@ const init = () => {
     visible: true,
     title: "OSMStandard",
   });
-
   map.addLayer(openstreetMapStandardLayer);
 
   const IndiaGeoJSON = new ol.layer.VectorImage({
@@ -38,9 +42,9 @@ const init = () => {
   });
 
   const iconStyle = new ol.style.Style({
+    // The following code will add a marker on the longest edge
     geometry: function (feature) {
       let geometry = feature.getGeometry();
-      console.log("geometry", geometry.getCoordinates()[0]);
       // find midpoint of longest line
       let coords = geometry.getCoordinates()[0];
       let ax = 0;
@@ -62,6 +66,46 @@ const init = () => {
     }),
   });
 
+  const rectangleStyle = new ol.style.Style({
+    geometry: function (feature) {
+      let geometry = feature.getGeometry();
+      // find midpoint of longest line
+      let coords = geometry.getCoordinates()[0];
+      let ax = 0;
+      let ay = 0;
+      let ax1 = 0;
+      let ay1 = 0;
+      let maxLength = -1;
+      for (var i = 0; i < coords.length; i++) {
+        let dx = coords[(i + 1) % coords.length][0] - coords[i][0];
+        let dy = coords[(i + 1) % coords.length][1] - coords[i][1];
+        if (dx * dx + dy * dy > maxLength) {
+          maxLength = dx * dx + dy * dy;
+          ax = coords[i][0] + dx / 2;
+          ay = coords[i][1] + dy / 2;
+        }
+      }
+      return new ol.geom.Polygon([
+        [
+          [ax, ay],
+          [ax + 3e6, ay],
+          [ax + 3e6, ay + 3e6],
+          [ax, ay + 3e6],
+        ],
+      ]);
+    },
+    // image: new ol.style.Icon({
+    //   src: "./data/static_images/marker.png",
+    // }),
+    fill: new ol.style.Fill({
+      color: "#000",
+    }),
+    stroke: new ol.style.Stroke({
+      color: "#0000",
+      width: 2,
+    }),
+  });
+
   const polygonStyle = new ol.style.Style({
     fill: new ol.style.Fill({
       color: "#fff",
@@ -72,10 +116,8 @@ const init = () => {
     }),
   });
 
-  const rectangleStyle = () => {};
-
-  IndiaGeoJSON.setStyle([polygonStyle, iconStyle]);
-
+  IndiaGeoJSON.setStyle([polygonStyle, rectangleStyle]);
   map.addLayer(IndiaGeoJSON);
 };
+
 window.onload = init;
